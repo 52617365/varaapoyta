4284e039b6d93426df777ec8bfbe027bbea59b10
52617365 <rasengithub@protonmail.com>
2022-06-28 09:35:18 +0300

    Footer and font change.

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,13 +4,12 @@ import Image from 'next/image'
 function Footer() {
     return (
         <div className={"fixed left-0 bottom-0 w-full text-center"}>
-            <p className={""}>Created by Rase</p>
-            <Link className={""} href="https://github.com/52617365">
-                <Image src={"/github.png"} alt={"Github Icon"} width={50} height={50}/>
+            <p>Created by Rase</p>
+            <Link href="https://github.com/52617365">
+                <Image src={"/github.png"} alt={"Github Icon"} width={40} height={40}/>
             </Link>
         </div>
     )
 }
-
 
 export default Footer;
