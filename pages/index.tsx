73d28013061c3f254d668a7531b81d4bae9e0932
52617365 <rasengithub@protonmail.com>
2022-06-28 14:25:19 +0300

    Added introduction text that is made with react spring.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
-import {useState} from 'react';
 import type {NextPage} from 'next'
 import SiteFooter from "../components/SiteFooter"
 import SiteHead from "../components/SiteHead";
 import Form from "../components/Form";
+import Introduction from "../components/Introduction"
 
 const Home: NextPage = () => {
     return (
         <>
             <SiteHead/>
+            <Introduction/>
             <Form/>
             <SiteFooter/>
         </>
