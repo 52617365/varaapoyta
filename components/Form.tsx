4a0f2b55837ffd2b7dfe3c6c1e5280a2f5ebef3e
52617365 <rasengithub@protonmail.com>
2022-06-28 13:04:11 +0300

    Started using daisyui and added new checkboxes.
    New checkboxes remember state.

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,8 +1,4 @@
-import FormControlLabel from '@mui/material/FormControlLabel';
 import React from "react";
-import Checkbox from "@mui/material/Checkbox";
-import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
-import Favorite from "@mui/icons-material/Favorite";
 
 function Form() {
     const [state, setState] = React.useState({
@@ -19,34 +15,42 @@ function Form() {
     };
     const {tulinen, mieto, kirpea} = state;
 
+    const [buttonLoading, setButtonLoading] = React.useState(false);
+
+    function setButton() {
+        setButtonLoading(true)
+    }
+    console.log(`tulinen: ${tulinen}`)
+    console.log(`mieto: ${mieto}`)
+    console.log(`Kirpea: ${kirpea}`)
     return (
-        <div className={"flex flex-col justify-center items-center"}>
-            <div>
-                <h1>Valitse mieltymyksiesi mukaan</h1>
+        <div className={"flex flex-col justify-center items-center pt-20"}>
+            <h1>Valitse mieltymyksiesi mukaan</h1>
+            <div className={"text-center pt-5 border-4 rounded-md border-indigo-600 "}>
                 <div
-                    className="text-center w-100 border-4 rounded-md border-indigo-600 grid gap-3 grid-cols-3 grid-rows-3">
-                    <FormControlLabel
-                        control={
-                            <Checkbox icon={<FavoriteBorder/>} checkedIcon={<Favorite/>} checked={tulinen}
-                                      onChange={handleChange} name="tulinen"/>
-                        }
-                        label="Tulinen"
-                    />
-                    <FormControlLabel
-                        control={
-                            <Checkbox icon={<FavoriteBorder/>} checkedIcon={<Favorite/>} checked={mieto}
-                                      onChange={handleChange} name="mieto"/>
-                        }
-                        label="Mieto"
-                    />
-                    <FormControlLabel
-                        control={
-                            <Checkbox icon={<FavoriteBorder/>} checkedIcon={<Favorite/>} checked={kirpea}
-                                      onChange={handleChange} name="kirpea"/>
-                        }
-                        label="Kirpeä"
-                    />
+                    className="w-100 grid gap-3 grid-cols-3 grid-rows-3">
+                    <div className={"form-control"}>
+                        <label className="label cursor-pointer">
+                            <span className="label-text">Tulinen</span>
+                            <input type="checkbox" defaultChecked={tulinen} onChange={handleChange} name={"tulinen"} className="checkbox"/>
+                        </label>
+                    </div>
+                    <div className={"form-control"}>
+                        <label className="label cursor-pointer">
+                            <span className="label-text">Mieto</span>
+                            <input type="checkbox" defaultChecked={mieto} onChange={handleChange} name={"mieto"} className="checkbox"/>
+                        </label>
+                    </div>
+                    <div className={"form-control"}>
+                        <label className="label cursor-pointer">
+                            <span className="label-text">Kirpeä</span>
+                            <input type="checkbox" defaultChecked={kirpea} onChange={handleChange} name={"kirpea"} className="checkbox"/>
+                        </label>
+                    </div>
                 </div>
+                {/* TODO: Make button go back to false state when fetching is done.*/}
+                {buttonLoading ? <button className="btn loading">Ladataan...</button> :
+                    <button onClick={setButton} type="submit" className="btn">Etsi</button>}
             </div>
         </div>
     )
