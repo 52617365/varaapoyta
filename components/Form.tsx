62dad292209e299485ed8284738ec8c7eb0e001f
Rase <rasengithub@protonmail.com>
2022-06-28 10:59:01 +0300

    Form borders centered now.

--- components/Form.tsx
+++ components/Form.tsx
@@ -20,29 +20,33 @@ function Form() {
     const {tulinen, mieto, kirpea} = state;
 
     return (
-        <div className={"w-full"}>
-            <div className="w-1/2 grid gap-4 grid-cols-3 grid-rows-3">
-                <FormControlLabel
-                    control={
-                        <Checkbox icon={<FavoriteBorder/>} checkedIcon={<Favorite/>} checked={tulinen}
-                                  onChange={handleChange} name="tulinen"/>
-                    }
-                    label="Tulinen"
-                />
-                <FormControlLabel
-                    control={
-                        <Checkbox icon={<FavoriteBorder/>} checkedIcon={<Favorite/>} checked={mieto}
-                                  onChange={handleChange} name="mieto"/>
-                    }
-                    label="Mieto"
-                />
-                <FormControlLabel
-                    control={
-                        <Checkbox icon={<FavoriteBorder/>} checkedIcon={<Favorite/>} checked={kirpea}
-                                  onChange={handleChange} name="kirpea"/>
-                    }
-                    label="Kirpeä"
-                />
+        <div className={"flex flex-col justify-center items-center"}>
+            <div>
+                <h1>Valitse mieltymyksiesi mukaan</h1>
+                <div
+                    className="text-center w-100 border-4 rounded-md border-indigo-600 grid gap-3 grid-cols-3 grid-rows-3">
+                    <FormControlLabel
+                        control={
+                            <Checkbox icon={<FavoriteBorder/>} checkedIcon={<Favorite/>} checked={tulinen}
+                                      onChange={handleChange} name="tulinen"/>
+                        }
+                        label="Tulinen"
+                    />
+                    <FormControlLabel
+                        control={
+                            <Checkbox icon={<FavoriteBorder/>} checkedIcon={<Favorite/>} checked={mieto}
+                                      onChange={handleChange} name="mieto"/>
+                        }
+                        label="Mieto"
+                    />
+                    <FormControlLabel
+                        control={
+                            <Checkbox icon={<FavoriteBorder/>} checkedIcon={<Favorite/>} checked={kirpea}
+                                      onChange={handleChange} name="kirpea"/>
+                        }
+                        label="Kirpeä"
+                    />
+                </div>
             </div>
         </div>
     )
