c8083f6c293f8b95a0139cee49ed0d7b924974ab
Rase <rasengithub@protonmail.com>
2022-06-29 23:08:41 +0300

    Added a game changing background theme.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,10 @@ import Link from "next/link"
 
 const Home: NextPage = () => {
     return (
-        <Link className="relative w-full" href={"/login"}>
-            <button className="absolute m-auto top-0 left-0 right-0 bottom-0 btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">Kirjaudu sisään
+        <Link className="relative" href={"/login"}>
+            <button
+                className="absolute m-auto top-0 left-0 right-0 bottom-0 btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">Kirjaudu
+                sisään
                 tallentaaksesi mieltymyksesi
             </button>
         </Link>
