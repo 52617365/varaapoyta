ad785af727e879f87cc2b0831c89a4e61d0dffd7
52617365 <rasengithub@protonmail.com>
2022-06-29 22:29:02 +0300

    Made pages for login and settings, both pretty empty right now but will be filled in the future.

---
+++ pages/settings.tsx
@@ -0,0 +1,15 @@
+import SiteHead from "../components/SiteHead";
+import Form from "../components/Form";
+import SiteFooter from "../components/SiteFooter";
+
+function Settings() {
+    return (
+        <>
+            <SiteHead/>
+            <Form/>
+            <SiteFooter/>
+        </>
+    )
+}
+
+export default Settings;
