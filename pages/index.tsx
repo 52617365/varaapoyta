b4946e5405a77b6aaaf527ead0e566db563a84d6
Rase <rasengithub@protonmail.com>
2022-06-30 09:36:11 +0300

    Button is now 50% of page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,10 @@ import Link from "next/link"
 
 const Home: NextPage = () => {
     return (
-        <Link className="relative" href={"/login"}>
+        <Link className="relative w-full" href={"/login"}>
             <button
-                className="absolute m-auto top-0 left-0 right-0 bottom-0 btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">Kirjaudu
-                sisään
-                tallentaaksesi mieltymyksesi
+                className="w-1/2 absolute m-auto top-0 left-0 right-0 bottom-0 btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">
+                Kirjaudu sisään tallentaaksesi mieltymyksesi
             </button>
         </Link>
     )
