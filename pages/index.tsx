2506d94329827aef50fbd42a26e90a32f944132d
52617365 <rasengithub@protonmail.com>
2022-06-30 10:08:43 +0300

    Starting screen is ready, deleted login cuz it was redundant, I'm going to send to login str8 from the first index page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,44 @@
 import type {NextPage} from 'next'
 import Link from "next/link"
+import Image from "next/image"
+import SiteFooter from "../components/SiteFooter";
 
 const Home: NextPage = () => {
     return (
-        <Link className="relative w-full" href={"/login"}>
-            <button
-                className="w-1/2 absolute m-auto top-0 left-0 right-0 bottom-0 btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">
-                Kirjaudu sisään tallentaaksesi mieltymyksesi
-            </button>
-        </Link>
+        <>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content text-center">
+                    <div className="max-w-md pr-5">
+                        <h1 className="text-5xl font-bold">Moikka,</h1>
+                        <p className="py-6">Aloita pöytävaraaminen kirjautumalla sisäan, ja tallentamalla
+                            mieltymyksesi.</p>
+                        {/*TODO: Add different paths to different oauth logins here*/}
+                        <span>
+                        <Link href={"/login"}>
+                            <button className="btn btn-square">
+                                <Image alt="gmail logo" src={"/pictures/gmail.svg"} width={50} height={50}/>
+                            </button>
+                        </Link>
+                    </span>
+                        <span>
+                        <Link href={"/login"}>
+                            <button className="btn btn-square">
+                                <Image alt="outlook logo" src={"/pictures/outlook.png"} width={50} height={50}/>
+                            </button>
+                        </Link>
+                    </span>
+                        <span>
+                        <Link href={"/login"}>
+                            <button className="btn btn-square">
+                                <Image alt="github logo" src={"/pictures/github.svg"} width={50} height={50}/>
+                            </button>
+                        </Link>
+                    </span>
+                    </div>
+                </div>
+            </div>
+            <SiteFooter/>
+        </>
     )
 }
 export default Home
