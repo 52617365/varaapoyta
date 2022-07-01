2b95e672c07a9d6fd62f9e6a57929eca16100bfc
Rase <rasengithub@protonmail.com>
2022-07-01 10:40:33 +0300

    Checkbox component now adds the event inside the attribute but it does not yet work.

--- components/Checkbox.tsx
+++ components/Checkbox.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-function Checkbox({name}: {name: string}) {
+function Checkbox({name, handler}: {name: string, handler: any}) {
     return (
         <div className={"form-control"}>
             <label className="label cursor-pointer">
                 <span className="label-text">{name}</span>
                 {/*defaultChecked={mieto} onChange={handleChange}*/}
-                <input type="checkbox" name={name}
+                <input onChange={handler} type="checkbox" name={name}
                        className="checkbox"/>
             </label>
         </div>
