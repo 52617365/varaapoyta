9a861ca3460b3b991132ea8bd5c2ba50349619c9
Rase <rasengithub@protonmail.com>
2022-07-01 10:38:33 +0300

    Varaa file now has a button that will in the future redirect to settings page.

--- pages/varaa.tsx
+++ pages/varaa.tsx
@@ -5,13 +5,17 @@ import SiteHead from "../components/SiteHead";
 function Varaa() {
     return (
         <>
-           <SiteHead/>
-            <div className="hero min-h-screen bg-base-200">
+            <SiteHead/>
+            <div className="relative hero min-h-screen bg-base-200">
                 <div className="hero-content text-center">
-                    <div className="max-w-md pr-5">
+                    <div className="absolute top-1/3 max-w-md pr-5">
                         {/*TODO: Add different paths to different oauth logins here*/}
-                        <h1 className={"text-xl pb-5"}>Raflaamo varaaja</h1>
-                        <Button/>
+                        <h1 className={"text-xl pb-10"}>Raflaamo varaaja</h1>
+                        <div className={"grid gap-10"}>
+                            <Button text={"Varaa pöytäsi"}/>
+                            {/*TODO: Make this button go in top right corner.*/}
+                            <Button text={"Muuta asetuksiasi"}/>
+                        </div>
                     </div>
                 </div>
             </div>
