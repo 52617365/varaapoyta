eaa0949e89e5d745631b7f14cb688feb4ee6e1ef
Rase <rasengithub@protonmail.com>
2022-07-01 10:39:21 +0300

    Refactored checkbox generator into its own function because it will be used countless times in the settings page.

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -3,34 +3,44 @@ import SiteFooter from "../components/SiteFooter";
 import React from "react";
 import DropDown from "../components/Dropdown";
 import Checkbox from "../components/Checkbox";
+import Button from "../components/Button";
+
+function GenerateCheckboxes(items: string[], handler: Function) {
+    return (
+        items.map((item) => {
+            return (
+                <Checkbox key={item} handler={handler} name={item}/>
+            )
+        }))
+}
 
 function Settings({restaurants}: { restaurants: string[] }) {
-    const [valitutRavintolat, lisaaRavintola] = React.useState(
-        []
+    // in the end valitutRavintolat will contain all the user selected restaurants.
+    // valitutAsetukset will be an object of arrays, array will contain all of the settings from the corresponding key (key will be a setting section)
+    // E.g {restaurants: [res1, res2...]},
+    const [valitutAsetukset, lisaaAsetus] = React.useState(
+        {}
     );
-    const handleRavintola = (newValue: String) => {
-        // @ts-ignore
-        lisaaRavintola((ravintolat: String) => [...ravintolat, newValue]);
-    };
 
-    const restaurantBoxes = restaurants.map((restaurant) => {
-        return (
-            // eslint-disable-next-line react/jsx-key
-            <Checkbox name={restaurant}/>
-        )
-    })
+    function asetusHandler(key: string, asetus: string) {
+    //    TODO: Add functionality for settings
+    }
+
+    const restaurantBoxes = GenerateCheckboxes(restaurants, asetusHandler)
+
     return (
         <>
             <SiteHead/>
             <div className={"flex flex-col justify-center items-center h-full inset-x-0 top-0"}>
                 {/*TODO: Capture state of selected items.*/}
-
-                {/*Ravinolat settings*/}
+                {/*Ravintolat settings*/}
                 <DropDown
-                    handleChange={"asd"}
                     items={restaurantBoxes}
                     name={"Ravintolat"}
                 />
+                <div className={"absolute bottom-1/3"}>
+                    <Button text={"Tallenna asetukset"}/>
+                </div>
             </div>
             <SiteFooter/>
         </>
