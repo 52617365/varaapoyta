abcd2f8000e941ff9d5f73d4ce22ddf5df44b930
Rase <rasengithub@protonmail.com>
2022-07-02 10:25:13 +0300

    Dropdown avautuu oikealle, koska aijemmin se meni aina muiden asetuksien paalle.

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -1,6 +1,6 @@
 function DropDown({name, items}: { name: string, items: Array<JSX.Element> }) {
     return (
-        <div className="dropdown dropdown-hover">
+        <div className="dropdown dropdown-right dropdown-hover">
             <label tabIndex={0} className="btn m-1">{name}</label>
             <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
                 {/*TODO: Add all restaurants here*/}
