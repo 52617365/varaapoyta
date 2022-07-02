4f56a6c0a3b46d2dc808abecf0536c2f4f5d2c3e
Rase <rasengithub@protonmail.com>
2022-07-02 15:58:52 +0300

    Vaihdoin footerin ja headerin _app.tsx tiedostoon, jottei niita tarvitse joka filussa kutsua uusiksi.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,25 @@
 import '../styles/globals.css'
 import type {AppProps} from 'next/app'
 import {ThemeProvider} from 'next-themes'
+import Head from "next/head";
+import Link from "next/link";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import SiteFooter from "../components/SiteFooter"
 
+// TODO: Laita sivu toimimaan hyvin mobilen kanssa.
+// TODO: Loyda tapa kayttaa uudelleen yhta handleChange funkiota kaikkiin asetuksiin.
 function MyApp({Component, pageProps}: AppProps) {
     return (
         <ThemeProvider defaultTheme="night">
+            <Head>
+                <title>Varaapoyta</title>
+                <meta name="viewport" content="viewport-fit=cover"/>
+            </Head>
             <Component {...pageProps} />
+            <SiteFooter/>
         </ThemeProvider>
+
     )
 }
 
