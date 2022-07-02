f4adb78c384165bde72d539abf3ae20eb78bff0d
Rase <rasengithub@protonmail.com>
2022-07-02 10:01:40 +0300

    Vaihdoin checkbox componentista event handlerin pois koska todellisuudessa sita ei lainkaan tarvinnut.

--- components/Checkbox.tsx
+++ components/Checkbox.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-function Checkbox({name, handler}: {name: string, handler: any}) {
+function Checkbox({name}: {name: string}) {
     return (
         <div className={"form-control"}>
             <label className="label cursor-pointer">
                 <span className="label-text">{name}</span>
                 {/*defaultChecked={mieto} onChange={handleChange}*/}
-                <input onChange={handler} type="checkbox" name={name}
+                <input type="checkbox" name={name}
                        className="checkbox"/>
             </label>
         </div>
