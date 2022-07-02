ef039ece52d3c7a9a359c9585d8d55a3fd9c42f2
52617365 <rasengithub@protonmail.com>
2022-07-02 10:26:43 +0300

    Vaihdoin nimen settings -> asetukset, koska tama aplikaatio on suomalaisille suunnattu.

--- pages/asetukset.tsx
+++ pages/asetukset.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import DropDown from "../components/Dropdown";
 import Checkbox from "../components/Checkbox";
 import Button from "../components/Button";
+import Link from "next/link";
 
 function GenerateCheckboxes(items: string[]) {
     return (
@@ -14,7 +15,7 @@ function GenerateCheckboxes(items: string[]) {
         }))
 }
 
-function Settings({restaurants}: { restaurants: string[] }) {
+function Asetukset({restaurants}: { restaurants: string[] }) {
     const [settings, setSettings] = React.useState({
         restaurants: [],
         //    TODO: Insert other wanted settings in here and do the same for all of them.
@@ -48,22 +49,27 @@ function Settings({restaurants}: { restaurants: string[] }) {
     return (
         <>
             <SiteHead/>
-            <div className={"flex flex-col justify-center items-center h-full inset-x-0 top-0"}>
-                {/*TODO: Capture state of selected items.*/}
-                {/*Ravintolat settings*/}
-                <div onChange={handleChange}>
-                    <DropDown
-                        items={restaurantBoxes}
-                        name={"Ravintolat"}
-                    />
-
-                    {/*TODO: Once user has saved settings, make button go back into the false state (not laoding)*/}
-                    <div className={"absolute bottom-1/3"}>
-                        <Button text={"Tallenna asetukset"}/>
+            <div className="relative hero min-h-screen bg-base-200">
+                <div className="hero-content text-center">
+                    <div className="absolute top-1/3 max-w-md pr-5">
+                        <h1 className={"text-xl pb-10"}>Asetukset</h1>
+                        <div onChange={handleChange} className={"grid gap-10"}>
+                            <DropDown
+                                items={restaurantBoxes}
+                                name={"Ravintolat"}
+                            />
+                            {/*TODO: Once user has saved settings, make button go back into the false state (not loading)*/}
+                            <Button text={"Tallenna asetukset"}/>
+                            <Link href={"/varaa"}>
+                                <a>
+                                    <Button text={"Takaisin varaamaan"}/>
+                                </a>
+                            </Link>
+                        </div>
                     </div>
+                    <SiteFooter/>
                 </div>
             </div>
-            <SiteFooter/>
         </>
     )
 }
@@ -79,4 +85,4 @@ export async function getStaticProps() {
     }
 }
 
-export default Settings;
+export default Asetukset;
