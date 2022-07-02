a1f691df8da4ba4528cbed0079e3007e1b3d0449
Rase <rasengithub@protonmail.com>
2022-07-02 17:42:45 +0300

    Poistin jotain todo's jota olen jo suorittanut.

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,28 +12,27 @@ const Home: NextPage = () => {
                         <h1 className="text-5xl font-bold">Moikka,</h1>
                         <p className="py-6">Aloita pöytävaraaminen kirjautumalla sisäan, ja tallentamalla
                             mieltymyksesi.</p>
-                        {/*TODO: Add different paths to different oauth logins here*/}
                         <span>
-                        <Link href={"/login"}>
-                            <button className="btn btn-square">
-                                <Image alt="gmail logo" src={"/pictures/gmail.svg"} width={50} height={50}/>
-                            </button>
-                        </Link>
-                    </span>
+                            <Link href={"/login"}>
+                                <button className="btn btn-square">
+                                    <Image alt="gmail logo" src={"/pictures/gmail.svg"} width={50} height={50}/>
+                                </button>
+                            </Link>
+                       </span>
                         <span>
-                        <Link href={"/login"}>
-                            <button className="btn btn-square">
-                                <Image alt="outlook logo" src={"/pictures/outlook.png"} width={50} height={50}/>
-                            </button>
-                        </Link>
-                    </span>
-                        <span>
-                        <Link href={"/login"}>
-                            <button className="btn btn-square">
-                                <Image alt="github logo" src={"/pictures/github.svg"} width={50} height={50}/>
-                            </button>
-                        </Link>
-                    </span>
+                            <Link href={"/login"}>
+                                <button className="btn btn-square">
+                                    <Image alt="outlook logo" src={"/pictures/outlook.png"} width={50} height={50}/>
+                                </button>
+                            </Link>
+                        </span>
+                            <span>
+                                <Link href={"/login"}>
+                                    <button className="btn btn-square">
+                                        <Image alt="github logo" src={"/pictures/github.svg"} width={50} height={50}/>
+                                    </button>
+                                </Link>
+                        </span>
                     </div>
                 </div>
             </div>
