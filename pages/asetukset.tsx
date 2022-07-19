dfe84763b558beceb0d717de78ce2c1fe0b88f67
Mark Donalds <markdonalds@clit.com>
2022-07-19 13:50:03 +0300

    Commit before I rewrite this whole shit.

--- pages/asetukset.tsx
+++ pages/asetukset.tsx
@@ -4,6 +4,7 @@ import Checkbox from "../components/Checkbox";
 import Button from "../components/Button";
 import Link from "next/link";
 
+// TODO: Fix checkboxes not updating on first page load, instead they generate on refresh.
 function GenerateCheckboxes(items: string[], storage_items: string[]) {
     // At this time, storage_items will contain all the items from the local storage. So here we check if it's in the local storage, we mark the checkbox as checked, else its unchecked.
     return (
@@ -84,7 +85,7 @@ function Asetukset({ravintolat, kaupungit}: { ravintolat: string[], kaupungit: s
         if (kaupunki_lista.kaupungit.length > 1) {
             window.localStorage.setItem("varaapoyta_kaupungit", JSON.stringify(kaupunki_lista.kaupungit))
             const items = window.localStorage.getItem("varaapoyta_kaupungit")
-            if (items !== null) {
+            if (items != null) {
                 const parsed_items = JSON.parse(items)
                 const deduplicated_items = parsed_items.filter((c: any, index: any) => {
                     return parsed_items.indexOf(c) === index;
@@ -92,13 +93,13 @@ function Asetukset({ravintolat, kaupungit}: { ravintolat: string[], kaupungit: s
                 window.localStorage.setItem("varaapoyta_kaupungit", JSON.stringify(deduplicated_items))
             }
         }
-    }, [kaupunki_lista.kaupungit]);
+    }, [kaupunki_lista]);
 
     useEffect(() => {
         if (ravintola_lista.ravintolat.length > 1) {
             window.localStorage.setItem("varaapoyta_ravintolat", JSON.stringify(ravintola_lista.ravintolat))
             const items = window.localStorage.getItem("varaapoyta_ravintolat")
-            if (items !== null) {
+            if (items != null) {
                 const parsed_items = JSON.parse(items)
                 const deduplicated_items = parsed_items.filter((c: any, index: any) => {
                     return parsed_items.indexOf(c) === index;
@@ -106,7 +107,7 @@ function Asetukset({ravintolat, kaupungit}: { ravintolat: string[], kaupungit: s
                 window.localStorage.setItem("varaapoyta_ravintolat", JSON.stringify(deduplicated_items))
             }
         }
-    }, [ravintola_lista.ravintolat]);
+    }, [ravintola_lista]);
 
     useEffect(() => {
         const ravintolat_storage = JSON.parse(window.localStorage.getItem("varaapoyta_ravintolat") as string) || [];
@@ -115,9 +116,6 @@ function Asetukset({ravintolat, kaupungit}: { ravintolat: string[], kaupungit: s
         const kaupunki_storage = JSON.parse(window.localStorage.getItem("varaapoyta_kaupungit") as string) || [];
         lisaaKaupunki({kaupungit: kaupunki_storage});
     }, []);
-
-    console.log(ravintola_lista.ravintolat)
-    console.log(kaupunki_lista.kaupungit)
 
     return (
         <>
