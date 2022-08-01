bd43af5c8f7e7293419e515e034aa300eab4bce6
52617365 <rasengithub@protonmail.com>
2022-08-01 14:24:50 +0300

    Added own files for deserializing and serializing.

---
+++ backend/deserialize.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type response_top_level struct {
+	Data response_second_level `json:"data"`
+}
+
+type response_second_level struct {
+	ListRestaurantsByLocation response_third_level `json:"listRestaurantsByLocation"`
+}
+
+type response_third_level struct {
+	Edges response_fields `json:"edges"`
+}
+
+type response_fields struct {
+	Id          int             `json:"id"`
+	Name        string_field    `json:"name"`
+	Urlpath     string_field    `json:"urlPath"`
+	Address     address_fields  `json:"address"`
+	Features    features_fields `json:"features"`
+	Openingtime opening_fields  `json:"openingTime"`
+	Links       links_fields    `json:"links"`
+}
+
+type string_field struct {
+	Fi_FI string `json:"fi_FI"`
+}
+
+type address_fields struct {
+	Municipality string_field `json:"municipality"`
+	Street       string_field `json:"street"`
+	Zipcode      string       `json:"zipCode"`
+}
+
+type features_fields struct {
+	Accessible bool `json:"accessible"`
+}
+
+type opening_fields struct {
+	Restauranttime opening_fields_ranges `json:"restaurantTime"`
+	Kitchentime    opening_fields_ranges `json:"kitchenTime"`
+	Accessible     bool                  `json:"accessible"`
+}
+
+type opening_fields_ranges struct {
+	Ranges string `json:"ranges"`
+}
+
+type links_fields struct {
+	ReservationLink string_field `json:"tableReservationLocalized"`
+	HomepageLink    string_field `json:"homepageLocalized"`
+}
+
+// res_body should be indexed to ["data"]["listRestaurantsByLocation"]["edges"] before it's deserialized into a structure.
+func deserialize_response(res **http.Response) *response_top_level {
+	response_decoded := &response_top_level{}
+	err := json.NewDecoder((*res).Body).Decode(response_decoded)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return response_decoded
+}
---
+++ backend/serialize.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+)
+
+type payload_structure struct {
+	Operationname string              `json:"operationName"`
+	Variables     variables_structure `json:"variables"`
+	Query         string              `json:"query"`
+}
+type variables_structure struct {
+	First int             `json:"first"`
+	Input input_structure `json:"input"`
+	After string          `json:"after"`
+}
+type input_structure struct {
+	RestaurantType string            `json:"restaurantType"`
+	Locationname   string            `json:"locationName"`
+	Feature        feature_structure `json:"feature"`
+}
+type feature_structure struct {
+	Rentablevenues bool `json:"rentableVenues"`
+}
+
+func generate_and_serialize_payload() []byte {
+	// Making the post request payload here to raflaamo api.
+	payload := &payload_structure{
+		Operationname: "getRestaurantsByLocation",
+		Variables: variables_structure{
+			First: 470,
+			Input: input_structure{
+				RestaurantType: "ALL",
+				Locationname:   "Helsinki",
+				Feature: feature_structure{
+					Rentablevenues: false,
+				},
+			},
+			After: "eyJmIjowLCJnIjp7ImEiOjYwLjE3MTE2LC",
+		},
+		Query: "fragment Locales on LocalizedString {fi_FI\n }\n\nfragment Restaurant on Restaurant {\n  id\n  name {\n    ...Locales\n    }\n  urlPath {\n    ...Locales\n     }\n    address {\n    municipality {\n      ...Locales\n       }\n        street {\n      ...Locales\n       }\n       zipCode\n     }\n    features {\n    accessible\n     }\n  openingTime {\n    restaurantTime {\n      ranges {\n        start\n        end\n        endNextDay\n         }\n             }\n    kitchenTime {\n      ranges {\n        start\n        end\n        endNextDay\n              }\n             }\n    }\n  links {\n    tableReservationLocalized {\n      ...Locales\n        }\n    homepageLocalized {\n      ...Locales\n          }\n   }\n     \n}\n\nquery getRestaurantsByLocation($first: Int, $after: String, $input: ListRestaurantsByLocationInput!) {\n  listRestaurantsByLocation(first: $first, after: $after, input: $input) {\n    totalCount\n      edges {\n      ...Restaurant\n        }\n     }\n}",
+	}
+
+	// Turning map into json string thats ready to send here.
+	json_bytes, err := json.Marshal(payload)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return json_bytes
+}
