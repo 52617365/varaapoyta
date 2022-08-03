ded2e166ede17ff934d0e3e4277cb17ac6b44a19
52617365 <rasengithub@protonmail.com>
2022-08-03 15:26:02 +0300

    We're now capturing the correct restaurants that we need.

--- backend/main.go
+++ backend/main.go
@@ -9,18 +9,25 @@ import (
 // (value.Address.Municipality.Fi_FI)
 func main() {
 	data := getRestaurants()
-	getRestaurantsFromCity(&data, "HELSINKI")
+	city := "HELSINKI"
+	restaurants := getRestaurantsFromCity(&data, &city)
+	for _, restaurant := range restaurants {
+		print_string := fmt.Sprintf("%s %s", *restaurant.Name.Fi_FI, *restaurant.Address.Municipality.Fi_FI)
+		fmt.Println(print_string)
+	}
 	//	for _, value := range data {
 	//		print_string := fmt.Sprintf("%s %s %s %s %s %s %t %s %s", value.Id, *value.Name.Fi_FI, *value.Urlpath.Fi_FI, *value.Address.Municipality.Fi_FI, *value.Address.Street.Fi_FI, *value.Address.Zipcode, value.Features.Accessible, *value.Links.TableReservationLocalized.Fi_FI, *value.Links.HomepageLocalized.Fi_FI)
 	//		fmt.Println(print_string)
 	//	}
 }
 
-func getRestaurantsFromCity(restaurants *[]response_fields, city string) {
+// TODO: capture result.
+func getRestaurantsFromCity(restaurants *[]response_fields, city *string) []response_fields {
+	captured_restaurants := []response_fields{}
 	for _, restaurant := range *restaurants {
-		if strings.Contains(strings.ToLower(*restaurant.Address.Municipality.Fi_FI), strings.ToLower(city)) {
-			print_string := fmt.Sprintf("%s %s", *restaurant.Name.Fi_FI, *restaurant.Address.Municipality.Fi_FI)
-			fmt.Println(print_string)
+		if strings.Contains(strings.ToLower(*restaurant.Address.Municipality.Fi_FI), strings.ToLower(*city)) {
+			captured_restaurants = append(captured_restaurants, restaurant)
 		}
 	}
+	return captured_restaurants
 }
