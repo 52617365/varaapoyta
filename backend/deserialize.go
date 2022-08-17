c0b1b5675221f502bc7ae0fd362b4c26a25ce59a
52617365 <rasengithub@protonmail.com>
2022-08-17 13:26:14 +0300

    Deserialize.go now deserialized the time range into Start and End.

--- backend/deserialize.go
+++ backend/deserialize.go
@@ -7,25 +7,25 @@ import (
 )
 
 type response_top_level struct {
-	Data *response_second_level `json:"data"`
+	Data response_second_level `json:"data"`
 }
 
 type response_second_level struct {
-	ListRestaurantsByLocation *response_third_level `json:"listRestaurantsByLocation"`
+	ListRestaurantsByLocation response_third_level `json:"listRestaurantsByLocation"`
 }
 
 type response_third_level struct {
-	Edges *[]response_fields `json:"edges"`
+	Edges []response_fields `json:"edges"`
 }
 
 type response_fields struct {
-	Id          string           `json:"id"`
-	Name        *string_field    `json:"name"`
-	Urlpath     *string_field    `json:"urlPath"`
-	Address     *address_fields  `json:"address"`
-	Features    *features_fields `json:"features"`
-	Openingtime *opening_fields  `json:"openingTime"`
-	Links       *links_fields    `json:"links"`
+	Id          string          `json:"id"`
+	Name        string_field    `json:"name"`
+	Urlpath     string_field    `json:"urlPath"`
+	Address     address_fields  `json:"address"`
+	Features    features_fields `json:"features"`
+	Openingtime opening_fields  `json:"openingTime"`
+	Links       links_fields    `json:"links"`
 }
 
 type string_field struct {
@@ -33,9 +33,9 @@ type string_field struct {
 }
 
 type address_fields struct {
-	Municipality *string_field `json:"municipality"`
-	Street       *string_field `json:"street"`
-	Zipcode      string        `json:"zipCode"`
+	Municipality string_field `json:"municipality"`
+	Street       string_field `json:"street"`
+	Zipcode      string       `json:"zipCode"`
 }
 
 type features_fields struct {
@@ -43,24 +43,29 @@ type features_fields struct {
 }
 
 type opening_fields struct {
-	Restauranttime *opening_fields_ranges `json:"restaurantTime"`
-	Kitchentime    *opening_fields_ranges `json:"kitchenTime"`
+	Restauranttime opening_fields_ranges `json:"restaurantTime"`
+	Kitchentime    opening_fields_ranges `json:"kitchenTime"`
 }
 
 type opening_fields_ranges struct {
-	Ranges *interface{} `json:"ranges"`
+	// Ranges interface{} `json:"ranges"`
+	Ranges []ranges_times `json:"ranges"`
+}
+type ranges_times struct {
+	Start string `json:"start"`
+	End   string `json:"end"`
 }
 
 type links_fields struct {
-	TableReservationLocalized *string_field `json:"tableReservationLocalized"`
-	HomepageLocalized         *string_field `json:"homepageLocalized"`
+	TableReservationLocalized string_field `json:"tableReservationLocalized"`
+	HomepageLocalized         string_field `json:"homepageLocalized"`
 }
 
-func deserialize_response(res **http.Response) *response_top_level {
+func deserialize_response(res **http.Response) response_top_level {
 	response_decoded := &response_top_level{}
 	err := json.NewDecoder((*res).Body).Decode(response_decoded)
 	if err != nil {
 		log.Fatal(err)
 	}
-	return response_decoded
+	return *response_decoded
 }
