b33d7efaeb358ef0732bc854568a9a7e31cb07c0
52617365 <rasengithub@protonmail.com>
2022-08-21 11:02:36 +0300

    get_time_slots_from_graph_api now returns a slice of time_slots instead of the first index.
    This was accidentally left in after testing.

--- backend/raflaamo_graph_api_deserialize.go
+++ backend/raflaamo_graph_api_deserialize.go
@@ -8,10 +8,7 @@ import (
 type parsed_graph_data struct {
 	Name      string                 `json:"name"`
 	Intervals []parsed_interval_data `json:"intervals"` // were only interested in the first index.
-	//Seats     int                    `json:"seats"`
-	Id int `json:"id"`
-	//Env       interface{}            `json:"env:"`
-	//Host      string                 `json:"host"`
+	Id        int                    `json:"id"`
 }
 
 type parsed_interval_data struct {
@@ -20,11 +17,11 @@ type parsed_interval_data struct {
 	Color string `json:"color"` // Optional field, we can match this to see if the restaurant has available tables. (if not nil it does.)
 }
 
-func deserialize_graph_response(res **http.Response) *parsed_graph_data {
+func deserialize_graph_response(res **http.Response) []parsed_graph_data {
 	var response_decoded []parsed_graph_data
 	err := json.NewDecoder((*res).Body).Decode(&response_decoded)
 	if err != nil {
 		return nil
 	}
-	return &response_decoded[0]
+	return response_decoded
 }
