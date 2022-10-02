d602532c4b855ac42fd010df6077954d15fd614b
52617365 <rasengithub@protonmail.com>
2022-10-02 13:25:23 +0300

    Refactor #2, we filter bad restaurants out now.

--- backend/raflaamoRestaurantsApi/restaurantsApi.go
+++ backend/raflaamoRestaurantsApi/restaurantsApi.go
@@ -5,15 +5,17 @@ import (
 	"errors"
 	"log"
 	"net/http"
+	"strings"
 )
 
 type RaflaamoRestaurantsApi struct {
 	httpClient *http.Client
 	request    *http.Request
 	response   *http.Response
+	usersCity  string
 }
 
-func getRaflaamoRestaurantsApi() (*RaflaamoRestaurantsApi, error) {
+func getRaflaamoRestaurantsApi(city string) (*RaflaamoRestaurantsApi, error) {
 	httpClient := &http.Client{}
 	const data = `{"operationName":"getRestaurantsByLocation","variables":{"first":1000,"input":{"restaurantType":"ALL","locationName":"Helsinki","feature":{"rentableVenues":false}},"after":"eyJmIjowLCJnIjp7ImEiOjYwLjE3MTE2LCJvIjoyNC45MzI1OH19"},"query":"fragment Locales on LocalizedString {fi_FI }fragment Restaurant on Restaurant {  id  name {    ...Locales    }  address {    municipality {      ...Locales       }        street {      ...Locales       }       zipCode     }   openingTime {    restaurantTime {      ranges {        start        end             }             }    kitchenTime {      ranges {        start        end        endNextDay              }             }    }  links {    tableReservationLocalized {      ...Locales        }    homepageLocalized {      ...Locales          }   }     }query getRestaurantsByLocation($first: Int, $after: String, $input: ListRestaurantsByLocationInput!) {  listRestaurantsByLocation(first: $first, after: $after, input: $input) {    totalCount      edges {      ...Restaurant        }     }}"}`
 
@@ -29,15 +31,12 @@ func getRaflaamoRestaurantsApi() (*RaflaamoRestaurantsApi, error) {
 	return &RaflaamoRestaurantsApi{
 		httpClient: httpClient,
 		request:    req,
+		usersCity:  city,
 	}, nil
 }
 
-func noRestaurantsFound(restaurantsField *responseTopLevel) bool {
-	return len(restaurantsField.Data.ListRestaurantsByLocation.Edges) == 0
-}
-
-func (restaurantsApi *RaflaamoRestaurantsApi) getRestaurantsFromRaflaamoApi() (*responseTopLevel, error) {
-	restaurantsApi, err := getRaflaamoRestaurantsApi()
+func (restaurantsApi *RaflaamoRestaurantsApi) getRestaurantsFromRaflaamoApi(city string) ([]responseFields, error) {
+	restaurantsApi, err := getRaflaamoRestaurantsApi(city)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -50,16 +49,73 @@ func (restaurantsApi *RaflaamoRestaurantsApi) getRestaurantsFromRaflaamoApi() (*
 	if err != nil {
 		return nil, errors.New("there was an error connecting to the raflaamo api")
 	}
+
 	restaurantsApi.response = res
 
 	decodedRaflaamoRestaurants, err := restaurantsApi.deserializeRaflaamoRestaurantsResponse()
+
 	if err != nil {
 		return nil, errors.New("there was an error deserializing raflaamo API response")
 	}
 
-	if noRestaurantsFound(decodedRaflaamoRestaurants) {
-		return nil, errors.New("no restaurants found with the specified criteria")
-	}
+	validRestaurantsMatchingCriteria := restaurantsApi.filterBadRestaurantsOut(decodedRaflaamoRestaurants)
 
-	return decodedRaflaamoRestaurants, nil
+	// TODO: Handle return value being empty in caller.
+	return validRestaurantsMatchingCriteria, nil
+}
+
+//
+// 	A restaurant is considered "Bad" if:
+//		- Restaurants city is not from the provided city.
+//		- Restaurants reservation link does not exist or contains odd contents.
+//		- Restaurant does not contain opening times (Specified in the Ranges array).
+//
+func (restaurantsApi *RaflaamoRestaurantsApi) filterBadRestaurantsOut(structureContainingRestaurantData *responseTopLevel) []responseFields {
+	restaurantsApi.usersCity = strings.ToLower(restaurantsApi.usersCity)
+	arrayContainingRestaurantData := structureContainingRestaurantData.Data.ListRestaurantsByLocation.Edges
+
+	filteredRestaurantsFromProvidedCity := make([]responseFields, 0, 50)
+	for _, restaurant := range arrayContainingRestaurantData {
+		if restaurant.isBad(restaurantsApi.usersCity) {
+			continue
+		}
+
+		// Here we have done all of the checks we know to date. There might be more in the future once I figure them out.
+		filteredRestaurantsFromProvidedCity = append(filteredRestaurantsFromProvidedCity, restaurant)
+	}
+	return filteredRestaurantsFromProvidedCity
+}
+
+func (restaurant *responseFields) isBad(city string) bool {
+	if restaurant.cityDoesNotMatchUsersCity(city) {
+		return true
+	}
+	if restaurant.reservationLinkIsNotValid() {
+		return true
+	}
+	if restaurant.doesNotContainOpeningTimes() {
+		return true
+	}
+	return false
+}
+
+func (response *responseFields) doesNotContainOpeningTimes() bool {
+	restaurantsOpeningTimes := response.Openingtime.Restauranttime.Ranges
+	kitchensOpeningTimes := response.Openingtime.Kitchentime.Ranges
+
+	if restaurantsOpeningTimes == nil || kitchensOpeningTimes == nil {
+		return true
+	}
+	return false
+}
+
+func (response *responseFields) reservationLinkIsNotValid() bool {
+	return !strings.Contains(response.Links.TableReservationLocalized.FiFi, "https://s-varaukset.fi/online/reservation/fi/")
+}
+
+func (response *responseFields) cityDoesNotMatchUsersCity(usersCity string) bool {
+	response.Name.FiFi = strings.ToLower(response.Name.FiFi)
+	restaurantsName := response.Name.FiFi
+
+	return restaurantsName != usersCity
 }
