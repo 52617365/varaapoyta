30c1ba73302c9f79fd18eb7ed5fb2a1ffecb43de
52617365 <rasengithub@protonmail.com>
2022-10-10 09:46:11 +0300

    Made captureResults.go a bit clearer.
    Signed-off-by: 52617365 <rasengithub@protonmail.com>

--- backend/restaurants/captureResults.go
+++ backend/restaurants/captureResults.go
@@ -17,42 +17,42 @@ func removeIndexFromSlice[T any](slice []T, s int) []T {
 func GetRestaurantsAndCollectResults(city string, amountOfEaters string) []raflaamoRestaurantsApi.ResponseFields {
 	restaurantsInstance := getRestaurants(city, amountOfEaters)
 	raflaamoRestaurants := restaurantsInstance.getRestaurantsAndAvailableTables()
-	iterateRestaurants(raflaamoRestaurants)
-	return raflaamoRestaurants
-}
-
-func iterateRestaurants(raflaamoRestaurants []raflaamoRestaurantsApi.ResponseFields) {
 	for index := range raflaamoRestaurants {
-		restaurant := &raflaamoRestaurants[index] // Else it won't actually be a ptr to it.
-		err := <-restaurant.GraphApiResults.Err
-		if err != nil {
-			continue
-		}
-		timeSlotsForRestaurant := iterateAndCaptureRestaurantTimeSlots(restaurant)
-		// We want to remove restaurants that don't have available time slots.
-		if len(timeSlotsForRestaurant) == 0 {
+		restaurant := &raflaamoRestaurants[index] // or else it's not an actual ptr.
+
+		timeSlotsForRestaurants := captureTimeSlotsForRestaurant(restaurant)
+		if len(timeSlotsForRestaurants) == 0 { // We want to remove restaurants that don't have available time slots.
 			removeIndexFromSlice(raflaamoRestaurants, index)
 			continue
 		}
-		restaurant.AvailableTimeSlots = timeSlotsForRestaurant
-
-		slices.Sort(restaurant.AvailableTimeSlots)
+		restaurant.AvailableTimeSlots = timeSlotsForRestaurants
 	}
+	return raflaamoRestaurants
+}
+
+func captureTimeSlotsForRestaurant(raflaamoRestaurant *raflaamoRestaurantsApi.ResponseFields) []string {
+	err := <-raflaamoRestaurant.GraphApiResults.Err
+	if err != nil {
+		return nil
+	}
+	timeSlotsForRestaurant := iterateAndCaptureRestaurantTimeSlots(raflaamoRestaurant)
+	slices.Sort(timeSlotsForRestaurant)
+	return timeSlotsForRestaurant
 }
 
 // iterateAndCaptureRestaurantTimeSlots captures the results from a channel because we send it over the network as JSON.
 func iterateAndCaptureRestaurantTimeSlots(restaurant *raflaamoRestaurantsApi.ResponseFields) []string {
 	availableTimeSlots := make([]string, 0, 50)
-	for result := range restaurant.GraphApiResults.AvailableTimeSlotsBuffer {
-		if GraphApiResponseHadNoTimeSlots(result) {
+	for availableTimeSlot := range restaurant.GraphApiResults.AvailableTimeSlotsBuffer {
+		if GraphApiResponseHadNoTimeSlots(availableTimeSlot) {
 			continue
 		}
-		availableTimeSlots = append(availableTimeSlots, result)
+		availableTimeSlots = append(availableTimeSlots, availableTimeSlot)
 	}
 	return availableTimeSlots
 }
 
-// In other words, if graph API response had the "transparent" field set.
+// GraphApiResponseHadNoTimeSlots if graph API response had the "transparent" field set.
 func GraphApiResponseHadNoTimeSlots(timeSlotResult string) bool {
 	// This exists because some time slots might have "transparent" field set aka no time slots found.
 	// And we are forced to send something down the channel or else it will keep waiting forever expecting n items to iterate.
